116a00ff583787bdf39813a60bae8952b9f0d221
Chayakorn Pattanachairoj <chayakorn.pat@axonstech.com>
2023-07-15 08:08:24 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,32 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Prediction of MBTI')
 
-st.write('Hello world!')
+title = st.text_input('Input', '')
+st.write('Input is', title)
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import nltk
+import re
+from tqdm import tqdm
+from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score,classification_report
+import warnings
+warnings.filterwarnings('ignore')
+
+data=pd.read_csv('mbti_1.csv')
+print(data)
